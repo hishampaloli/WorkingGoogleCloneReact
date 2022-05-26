a3cea4f03741231a2afb1f80fbe290f9aef2b4b1
hishampaloli <88199878+hishampaloli@users.noreply.github.com>
2022-05-26 19:34:49 +0530

    Update GoogleImg.js

--- src/Constants/GoogleImg.js
+++ src/Constants/GoogleImg.js
@@ -14,7 +14,7 @@ const options = {
       'X-User-Agent': 'desktop',
       'X-Proxy-Location': 'EU',
       'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-      'X-RapidAPI-Key': 'ff85a0dbafmshe35a6ef7d310686p14f37cjsn1606943bd8d2'
+      'X-RapidAPI-Key': 'im Not A Fool'
     }
   };
   
