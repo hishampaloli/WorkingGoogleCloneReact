ff1a7e5c27b843b337d303e90e5e9f903865c48b
hishampaloli <88199878+hishampaloli@users.noreply.github.com>
2022-05-26 19:35:14 +0530

    Update GoogleNews.js

--- src/Constants/GoogleNews.js
+++ src/Constants/GoogleNews.js
@@ -15,7 +15,7 @@ function GoogleNews(term) {
           'X-User-Agent': 'desktop',
           'X-Proxy-Location': 'EU',
           'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-          'X-RapidAPI-Key': 'ff85a0dbafmshe35a6ef7d310686p14f37cjsn1606943bd8d2'
+          'X-RapidAPI-Key': 'im Not A Fool'
         }
       };
   
